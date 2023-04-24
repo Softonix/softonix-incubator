817de8d328b5aec21bae22fc3e5e23af89e3b34d
OlegBarylko <olegbarylko81@gmail.com>
2023-04-24 10:49:23 +0300

    add homework

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="max-w-[1440px] p-6">
-    <h3 class="font-medium m-0">Contact list</h3>
+    <div class="flex space-x-6 items-center">
+      <h3 class="font-medium m-0">Contact list</h3>
+      <button class="font-medium bg-[#ffffff] p-2 rounded-full" @click="addContact()">
+        <img src="@/assets/images/plus-image.png" alt="">
+      </button>
+    </div>
 
     <div class="contact-list grid-cols-[repeat(auto-fill,_minmax(320px,_1fr))] grid gap-5 my-5">
       <ContactItem
         v-for="(contact, index) in contacts"
-        :key="contact.id"
-        :contact="contact"
-        @delete="deleteContact(index)"
-        @save="onContactSave($event, index)"
-      />
+        :key="contact.id" :contact="contact"
+        @delete="deleteContact(index)" @save="onContactSave($event, index)"/>
     </div>
   </div>
 </template>
@@ -21,19 +23,19 @@ import ContactItem from '@/components/ContactItem.vue'
 
 const contacts = ref<IContact[]>([
   {
-    id: 1,
+    id: 0,
     name: 'Esther Howard',
     description: 'Forward Response Developer',
     image: 'https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'
   },
   {
-    id: 2,
+    id: 1,
     name: 'Jane Cooper',
     description: 'Regional Paradigm Technician Regional Paradigm Technician Regional Paradigm Technician',
     image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'
   },
   {
-    id: 3,
+    id: 2,
     name: 'Cody Fisher',
     description: 'Product Directives Officer',
     image: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60'
@@ -47,4 +49,15 @@ function deleteContact (index: number) {
 function onContactSave (contact: IContact, index: number) {
   contacts.value[index] = { ...contact }
 }
+
+function addContact (): void {
+  const newContact: IContact = {
+    id: contacts.value.length,
+    name: '',
+    description: '',
+    image: 'https://firebasestorage.googleapis.com/v0/b/lapiec-6c366.appspot.com/o/user.png?alt=media&token=2096ff6f-f1c8-441f-baa1-f7b6aac73cdb'
+  }
+  contacts.value.unshift(newContact)
+}
+
 </script>
--- src/components/ContactItem.vue
+++ src/components/ContactItem.vue
@@ -3,14 +3,20 @@
     <div class="p-6 pb-2">
       <div class="flex">
         <div class="flex-grow text-sm truncate">
-          <template v-if="editMode">
+          <template v-if="editMode || validation()">
             <input
               ref="inputRef"
               v-model="localContact.name"
               type="text"
               class="block font-medium w-full"
+              placeholder="Full Name"
             >
-            <input v-model="localContact.description" type="text" class="block mt-1 text-gray w-full">
+            <input
+              v-model="localContact.description"
+              placeholder="Professional position"
+              type="text" class="block mt-1 text-gray w-full"
+            >
+            <input v-model="localContact.image" placeholder="URL" type="text" class="block mt-1 w-full">
           </template>
 
           <template v-else>
@@ -26,28 +32,35 @@
         >
       </div>
       <div class="flex justify-end mt-2 gap-2">
-        <template v-if="editMode">
+        <template v-if="!editMode && !validation()">
           <span
             class="text-blue-500 font-medium text-xs cursor-pointer hover:underline"
-            @click="editMode = false"
-          >Cancel</span>
-
+            @click="triggerEditMode"
+          >Edit</span>
+          <span
+            class="text-red-500 font-medium text-xs cursor-pointer hover:underline"
+            @click="$emit('delete')"
+          >Delete</span>
+        </template>
+        <template v-else-if="validation()">
+          <span
+            class="text-red-500 font-medium text-xs cursor-pointer hover:underline"
+            @click="$emit('delete')"
+          >Delete</span>
           <span
             class="text-blue-500 font-medium text-xs cursor-pointer hover:underline"
             @click="onSave"
           >Save</span>
         </template>
-
         <template v-else>
           <span
-            class="text-blue-500 font-medium text-xs cursor-pointer hover:underline"
-            @click="triggerEditMode"
-          >Edit</span>
-
-          <span
             class="text-red-500 font-medium text-xs cursor-pointer hover:underline"
-            @click="$emit('delete')"
-          >Delete</span>
+            @click="editMode =false"
+          >Cancel</span>
+          <span
+            class="text-blue-500 font-medium text-xs cursor-pointer hover:underline"
+            @click="onSave"
+          >Save</span>
         </template>
       </div>
     </div>
@@ -85,7 +98,7 @@ const inputRef = ref<HTMLInputElement>()
 const localContact = ref<Omit<IContact, 'id'>>({
   name: '',
   description: '',
-  image: ''
+  image: 'https://firebasestorage.googleapis.com/v0/b/lapiec-6c366.appspot.com/o/user.png?alt=media&token=2096ff6f-f1c8-441f-baa1-f7b6aac73cdb'
 })
 
 const editMode = ref(false)
@@ -101,4 +114,12 @@ function onSave () {
   emit('save', localContact.value)
   editMode.value = false
 }
+
+function validation (): boolean {
+  if (props.contact.name && props.contact.description) {
+    return false
+  }
+  return true
+}
+
 </script>
